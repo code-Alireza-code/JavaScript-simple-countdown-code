3f0961cb20a2c13820c3b6c4c0ff249013137fcd
Alireza Mohammadi <alireza.mohammadi.m19>
2023-07-18 12:02:09 +0330

    code commited !

---
+++ main.js
@@ -0,0 +1,20 @@
+//* A simple countdown code
+
+function countDown(time, step = 1) {
+  const timer = () => {
+    const min = String(Math.trunc(time / 60)).padStart(2, 0);
+    const sec = String(time % 60).padStart(2, 0);
+    if (time === 0) {
+      clearInterval(intervalId);
+      console.log("click to resend OTP!");
+    } else {
+      console.log(`${min}:${sec}`);
+      time = time - step;
+    }
+  };
+  const delay = step * 1000;
+  timer();
+  const intervalId = setInterval(timer, delay);
+}
+//countDown(time, timeStep)
+countDown(15, 4);
